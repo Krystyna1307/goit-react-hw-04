4bf2944708dfb186b59fcdda7d830fe8790295b2
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-11-15 19:36:45 +0100

    get

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,14 @@ import axios from "axios";
 const ACCESS_KEY = "uIDxmkSbmKZab56d796f3xkULhRzGMhUmXAaOYp_104";
 
 export const fetchImages = async (query, page) => {
-  const response = await axios.get(
-    `https://api.unsplash.com/search/photos?client_id=${ACCESS_KEY}&query=${query}&page=${page}&per_page=100`
-  );
+  const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+    params: {
+      client_id: ACCESS_KEY,
+      query,
+      page,
+      per_page: 100,
+    },
+  });
   return response.data;
 };
 
